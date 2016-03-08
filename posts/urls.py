ee36e7c41f35271f7b809f3effb755033f580a76
Anna <annachem368@gmail.com>
2016-03-07 17:57:13 -0800

    add template, CRUD

---
+++ posts/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include,url
+from django.contrib import admin
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', "posts.views.posts_list"),
+    url(r'^create/$', "posts.views.posts_create"),
+    url(r'^detail/$', "posts.views.posts_detail"),
+    url(r'^update/$', "posts.views.posts_update"),
+    url(r'^delete/$', "posts.views.posts_delete"),
+    # url(r'^abc/$', "posts.views.posts_home"),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,35 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def posts_create(request):
+
+	return HttpResponse("<h1>posts_create<h1>")
+
+def posts_detail(request):
+	context = {
+		"title":"detail"
+	}
+		
+	return render(request,"index.html",context)
+
+def posts_list(request):
+	if request.user.is_authenticated():
+		context = {
+			"title":"My User List"
+		}
+	else:
+		context = {
+			"title":"List"
+		}
+
+	return render(request,"index.html",context)
+
+def posts_update(request):
+
+	return HttpResponse("<h1>posts_update<h1>")
+
+def posts_delete(request):
+
+	return HttpResponse("<h1>posts_delete<h1>")
